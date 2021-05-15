0b2828b4922fa2f39d81e1c1249b75a7f2f2d75e
Hetorus NL <hetorusnl@DebianVM>
2021-05-15 18:42:41 +0200

    added initial API server and updated readme

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Vocya - vocabulary shop
 
-Front-end for the vocjem data; dashboard and API
+Front-end for the vocjem data; dashboard and API.
+
+## API
+
+The API uses python3 and flask to host the API server.
+The API returns a (potentially empty) list of results in JSON format.
+The current API endpoint can be reached at: https://api.vocya.hetorus.nl/.
+
+### Example queries
+
+Query all words  
+https://api.vocya.hetorus.nl/words  
+Query words with id='2'  
+https://api.vocya.hetorus.nl/word/id/2  
+Query words with chapter='1'  
+https://api.vocya.hetorus.nl/word/chapter/1  
+Query words with search query='ho' (matching any value in the returned JSON)  
+https://api.vocya.hetorus.nl/search/word/ho
---
+++ api/api.py
@@ -0,0 +1,61 @@
+import json
+from flask import Flask, jsonify
+
+api = Flask(__name__)
+
+
+@api.route("/words")
+def all_words():
+    return jsonify(_get_vocabulary())
+
+
+@api.route("/word/id/<string:id>")
+def word_id(id):
+    vocabulary = _get_vocabulary()
+    res = list(filter(lambda a: a["id"] == id, vocabulary))
+    return jsonify(res)
+
+
+@api.route("/word/chapter/<string:id>")
+def word_chapter(id):
+    vocabulary = _get_vocabulary()
+    res = list(filter(lambda a: a["chapter"] == id, vocabulary))
+    return jsonify(res)
+
+
+@api.route("/search/word/<string:query>")
+def search_word(query):
+    vocabulary = _get_vocabulary()
+    res = []
+    for entry in vocabulary:
+        querywords_found = 0
+        for queryword in query.split(" "):
+            for key in entry.keys():
+                # search case-insensitive
+                if queryword.lower() in entry[key].lower():
+                    querywords_found += 1
+                    break
+        if querywords_found == len(query.split(" ")):
+            res.append(entry)
+    return jsonify(res)
+
+
+def _get_vocabulary():
+    with open("/opt/vocjem/dictionary.json") as f:
+        dictionary = json.load(f)
+    vocabulary = dictionary["vocabulary"]
+    # resolve the LUT, it might be better to do this after (optional) filtering
+    for entry in vocabulary:
+        new_keys = {}
+        for key in entry.keys():
+            if key in dictionary["lut"].keys():
+                lut = dictionary["lut"]
+                lutval = dictionary.get(lut.get(key))
+                if not lutval:
+                    continue
+                res = list(filter(lambda a: a["id"] == entry[key], lutval))
+                if not res:
+                    continue
+                new_keys[f"{key}_name"] = res[0].get("name")
+        entry.update(new_keys)
+    return vocabulary
