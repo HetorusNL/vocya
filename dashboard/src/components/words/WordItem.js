fa269337b95a9da2d0afdb9a318f6ff6c645d6ac
Hetorus NL <timkleinnijenhuis@hotmail.com>
2021-06-25 23:17:10 +0200

    added hiragana to the words cards

--- dashboard/src/components/words/WordItem.js
+++ dashboard/src/components/words/WordItem.js
@@ -2,12 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-const WordItem = ({ word: { id, chapter, chapter_name, dutch, romaji } }) => {
+const WordItem = ({
+  word: { id, chapter, chapter_name, dutch, hiragana, romaji },
+}) => {
   return (
     <div className="card text-left">
       <Link to={`/word/id/${id}`} className="text-dark">
         <p>{romaji}</p>
         <p>{dutch}</p>
+        <p>{hiragana}</p>
       </Link>
     </div>
   );
