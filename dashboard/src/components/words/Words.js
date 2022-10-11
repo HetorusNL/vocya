6868320e9baa733a350a7e28b9b8ea2af050f648
Tim Klein Nijenhuis <timkleinnijenhuis@hotmail.com>
2022-10-11 23:31:24 +0200

    added infinite scrolling to vocya to load 50 elements at a time

--- dashboard/src/components/words/Words.js
+++ dashboard/src/components/words/Words.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 import WordItem from "./WordItem";
 
 const Words = ({ words, loading }) => {
+  const [allWords, setAllWords] = useState([]);
+  const [shownWords, setShownWords] = useState([]);
+  const [maxElements, setMaxElements] = useState(0);
+  const wordsPerPage = 50;
+  const pixelsFromBottom = 100;
   const wordStyle = {
     display: "grid",
     maxWidth: "1500px",
@@ -13,11 +18,44 @@ const Words = ({ words, loading }) => {
     marginBottom: "1rem",
   };
 
+  // if words change, reset the max elements on the page to '1 page'
+  useEffect(() => {
+    setAllWords(words);
+    setMaxElements(wordsPerPage);
+  }, [words]);
+
+  // if allWords array or the maxElements change, update the shownWords
+  useEffect(() => {
+    setShownWords(allWords.slice(0, maxElements));
+  }, [allWords, maxElements]);
+
+  // handle scrolling; add additional elements when pixelsFromBottom is reached
+  useEffect(() => {
+    const handleScroll = (e) => {
+      const scrollTop = e.target.scrollingElement.scrollTop;
+      const scrollHeight = e.target.scrollingElement.scrollHeight;
+      const clientHeight = e.target.scrollingElement.clientHeight;
+      if (scrollTop + pixelsFromBottom >= scrollHeight - clientHeight) {
+        if (maxElements < allWords.length) {
+          setMaxElements(maxElements + wordsPerPage);
+        }
+      }
+    };
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => {
+      // this cleans up the event handler when the component unmounts
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [maxElements, allWords]);
+
   return (
-    <div style={wordStyle}>
-      {words.map((word) => (
-        <WordItem key={word.id} word={word} />
-      ))}
+    <div>
+      Search results: {allWords.length}, shown: {shownWords.length}
+      <div style={wordStyle}>
+        {shownWords.map((word) => (
+          <WordItem key={word.id} word={word} />
+        ))}
+      </div>
     </div>
   );
 };
