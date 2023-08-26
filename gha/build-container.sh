72a9fce3a1ef68f40aac5ccd50f0fc5d7789a369
HetorusNL <tim@hetorus.nl>
2023-08-27 00:22:46 +0200

    updated CI/CD for tags

--- gha/build-container.sh
+++ gha/build-container.sh
@@ -3,13 +3,20 @@
 # magic options for bash to make scripts safer
 set -Eeuxo pipefail
 
-# create a valid docker tag name from the branch name
-VALID_BRANCH_NAME=`echo $BRANCH_NAME | sed 's/[^a-zA-Z0-9]/-/g'`
-# if the branch is master, create an image using 'latest', otherwise the valid branch name
-if [[ $VALID_BRANCH_NAME == "master" ]]; then
-    DOCKER_TAG=latest
-else
-    DOCKER_TAG="test-$VALID_BRANCH_NAME"
+# check if we're running on a tag creation
+if [[ $REF_TYPE == "tag" ]]; then
+    # we're running on tag creation, set DOCKER_TAG to that tag
+    # make sure that the tag specified is valid
+    DOCKER_TAG=`echo $REF_NAME | sed 's/[^a-zA-Z0-9]/-/g'`
+else # we're running on a branch
+    # create a valid docker tag name from the branch name
+    VALID_BRANCH_NAME=`echo $REF_NAME | sed 's/[^a-zA-Z0-9]/-/g'`
+    # if the branch is master, create an image using 'latest', otherwise the valid branch name
+    if [[ $VALID_BRANCH_NAME == "master" ]]; then
+        DOCKER_TAG=latest
+    else
+        DOCKER_TAG="test-$VALID_BRANCH_NAME"
+    fi
 fi
 
 # build the docker image with the configured registry and tag
